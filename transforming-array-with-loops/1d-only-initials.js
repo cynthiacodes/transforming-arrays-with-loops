3fa5399fda6a0c6671b3dd6b4733d34dbe51cdc9
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-17 14:00:29 +0100

    add function and test case

---
+++ transforming-array-with-loops/1d-only-initials.js
@@ -0,0 +1,7 @@
+function onlyInitials(namesArray){
+    return 0;
+}
+
+// Test cases
+
+console.log(onlyInitials(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), 'output should be ["A.K.", "L.H.", "Y.M."]');
