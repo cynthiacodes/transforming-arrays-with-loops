4de602021e76f950984f2cf99662cd6ad3e39c34
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-17 17:14:47 +0100

    initial commit

---
+++ transforming-array-with-loops/3b-invert-numbers.js
@@ -0,0 +1,18 @@
+
+function invertNumbers(numbersArray) {
+
+    result = [];
+    
+    for (let numberIndex in numbersArray) {
+
+        const convertedNumber = numbersArray[numberIndex] * -1;
+
+        result.push(convertedNumber);
+    }
+
+    return result
+}
+
+
+// Test Cases
+console.log(invertNumbers([1, -3, 2, 8, -10]), 'output should be [-1, 3, -2, -8, 10]')
